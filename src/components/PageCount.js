496e6a4e9b30b43489bb7d9a9794033daa5aeb75
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-08-15 08:35:46 +0800

    fix problem

--- src/components/PageCount.js
+++ src/components/PageCount.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Pagination } from 'react-bootstrap'
 // import { paginationStyle } from '../style'
 
-const PageCount = ({ page, setPage, data }) => {
+const PageCount = ({ page, setPage }) => {
   const arrs = [1, 2, 3, 4, 5, 6]
 
   return (
@@ -10,7 +10,6 @@ const PageCount = ({ page, setPage, data }) => {
       {arrs.map(arr => (
         <Pagination.Item key={arr} title={`第1頁`} onClick={() => setPage(arr)}>{arr}</Pagination.Item>
       ))}
-      <div>{console.log(data.results.length)}</div>
     </Pagination>
   )
 }
