453291ceed654e255207b8fd66e1ec961c2e9085
Upfloor5GoOnTheWay <32410486+Ponpon55837@users.noreply.github.com>
2020-08-18 15:58:18 +0800

    remove auth0 , itis used to another project auth0withReact

--- src/App.js
+++ src/App.js
@@ -3,36 +3,22 @@ import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Planets from './components/Planets'
 import People from './components/People'
-import NotLogInPage from './LayoutComponents/NotLogInPage'
-import Header from './LayoutComponents/Header'
 // import { ReactQueryDevtools } from 'react-query-devtools'
 import { Container } from 'react-bootstrap'
-import { useAuth0 } from '@auth0/auth0-react'
 
 const App = () => {
-  const { user, isAuthenticated } = useAuth0()
   const [page, setPage] = useState('planets')
 
   return (
-
     <>
-      <Header />
-      {
-        isAuthenticated ?
-        (
-          <div>
-            <h1>Star Wars Info</h1>
-            <Navbar setPage={setPage} />
-              <Container className='content'>
-                { page === 'planets' ? <Planets /> : <People /> }
-              </Container>
-          </div>
-        ) :
-        <NotLogInPage />
-      }
+      <div>
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+          <Container className='content'>
+            { page === 'planets' ? <Planets /> : <People /> }
+          </Container>
+      </div>
       <Footer />
-
-
     </>
   )
 }
